3d0726281b9896e8a4b8e8575e0663e9d7ee60bb
apoorva <97716935+carokhan@users.noreply.github.com>
2025-04-11 19:16:14 -0400

    Update at 'Fri Apr 11 19:16:14 EDT 2025'

--- src/main/java/frc/robot/subsystems/led/LEDIOCandle.java
+++ src/main/java/frc/robot/subsystems/led/LEDIOCandle.java
@@ -1,23 +1,22 @@
 package frc.robot.subsystems.led;
 
 import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
-
+import com.ctre.phoenix.led.CANdleConfiguration;
 import frc.robot.subsystems.led.LEDConstants.Mode;
 
 public class LEDIOCandle implements LEDIO {
-    private final CANdle candle;
-    private final CANdleConfiguration config = new CANdleConfiguration();
-    private Mode mode = Mode.OFF;
+  private final CANdle candle;
+  private final CANdleConfiguration config = new CANdleConfiguration();
+  private Mode mode = Mode.OFF;
 
-    public LEDIOCandle() {
-        candle = new CANdle(LEDConstants.candleId);
+  public LEDIOCandle() {
+    candle = new CANdle(LEDConstants.candleId);
 
-        config.stripType = LEDStripType.RGB;
-        config.brightnessScalar = 1.0;
-        candle.configAllSettings(config);
+    config.stripType = LEDStripType.RGB;
+    config.brightnessScalar = 1.0;
+    candle.configAllSettings(config);
 
-        candle.setLEDs(255, 255, 255);
-    }
+    candle.setLEDs(255, 255, 255);
+  }
 }
