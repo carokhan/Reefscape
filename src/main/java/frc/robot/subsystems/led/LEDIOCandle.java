d69af09dc66fa1256d55306d3cbfc394e9f0dfa2
apoorva <97716935+carokhan@users.noreply.github.com>
2025-04-11 19:33:08 -0400

    Update at 'Fri Apr 11 19:33:07 EDT 2025'

--- src/main/java/frc/robot/subsystems/led/LEDIOCandle.java
+++ src/main/java/frc/robot/subsystems/led/LEDIOCandle.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.led;
 
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
-import com.ctre.phoenix.led.CANdle.VBatOutputMode;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import frc.robot.subsystems.led.LEDConstants.Mode;
 
@@ -14,11 +13,7 @@ public class LEDIOCandle implements LEDIO {
   public LEDIOCandle() {
     candle = new CANdle(LEDConstants.candleId, "rio");
 
-    config.statusLedOffWhenActive = true;
-    config.disableWhenLOS = true;
     config.stripType = LEDStripType.RGB;
-
-    config.vBatOutputMode = VBatOutputMode.Off;
     config.brightnessScalar = 1.0;
     candle.configAllSettings(config);
 
