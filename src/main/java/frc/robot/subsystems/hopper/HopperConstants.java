24467599743b26eeed23d5b891767ccb931b042c
apoorva <97716935+carokhan@users.noreply.github.com>
2025-04-11 18:07:12 -0400

    Update at 'Fri Apr 11 18:07:12 EDT 2025'

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -8,7 +8,7 @@ public class HopperConstants {
   public static final int current = 40;
   public static final double currentLower = 30;
   public static final double currentLowerTime = 0.5;
-  public static final boolean inverted = false;
+  public static final boolean inverted = true;
 
   public static final double gearing = 28.0 / 12.0;
   public static final double moi = 0.004;
